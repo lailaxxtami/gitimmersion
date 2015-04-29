44cc60c67b552879bd34fdfe85f29ad346945235
Tammy Nguyen <tnguy144@uncc.edu>
2015-04-28 22:56:33 -0400

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-# Default is "World"
-# Author: Tammy Nguyen (tnguy144@uncc.edu)
-=======
-# This is an unwanted but committed change
->>>>>>> d796a712ed316c60fee113209b31483eb48e6694
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
-
+puts "Hello, #{my_name}!"
