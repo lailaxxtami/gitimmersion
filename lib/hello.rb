5de85ba1241861586bb3cd416a52e9900a29cbd2
Tammy Nguyen <tnguy144@uncc.edu>
2015-04-28 22:50:02 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-# Default is "World"
-# Author: Tammy Nguyen (tnguy144@uncc.edu)
-=======
-# This is an unwanted but committed change
->>>>>>> d796a712ed316c60fee113209b31483eb48e6694
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
 
